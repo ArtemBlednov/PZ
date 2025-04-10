ad45b968df6bfd6450b8d6f52a301e4844a74944
ArtemBlednov <sitovartem752@gmail.com>
2025-04-10 18:41:52 +0300

    Practical task 2, version 1.2 fix

--- PZ - 12/PZ_12_2.py
+++ PZ - 12/PZ_12_2.py
@@ -5,8 +5,8 @@ import string
 def gen(line):
     yield from [i for i in line if i in string.ascii_letters + 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ']
 
-line_test = 'Строка с пробелами'
+line_test = 'Строка с пробелами 124 14 1 23 и цифрами :)) ?:;(?"(;?!'
 
 result = gen(line_test)
 
-print(list(result))
+print(' '.join(result))
