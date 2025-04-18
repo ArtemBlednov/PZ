c9152198adb0cf96395a7d4a8b99bf0e8c708d1b
ArtemBlednov <sitovartem752@gmail.com>
2025-04-18 12:53:41 +0300

    Practical task 1, restruct

--- PZ - 12/PZ_12_1.py
+++ PZ - 12/PZ_12_1.py
@@ -1,9 +1,26 @@
 """В последовательности на n целых чисел умножить элементы до n-1 на элемент
 n."""
 
-num = [1, 2, 3, 4, 5]
+import random
+
+while True:
+    try:
+        n = int(input('Введите последовательность n чисел: '))
+        break
+
+    except ValueError:
+        print('Неправильный тип данных!!! Введите целое число.')
 
 
+data = [random.randint(0, 10) for i in range(n)]
 
-mult = [i * i for i in range(1, len(num))]
+print('Список:')
+print(data)
+
+print('n - элемент: ')
+print(data[-1])
+
+mult = [i * data[-1] if i != data[-1] else i for i in data]
+
+print('Конечный список:')
 print(mult)
